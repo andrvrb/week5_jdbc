bcae8fa6ad21ce5ec83f577d5491c5f6085b2945
andrvrb <andr-pnz@mail.ru>
2023-10-12 21:41:03 +0300

    step 3

--- src/main/java/ru/egartech/jdbc/starter/JdbcRunner.java
+++ src/main/java/ru/egartech/jdbc/starter/JdbcRunner.java
@@ -2,28 +2,30 @@ package ru.egartech.jdbc.starter;
 
 import ru.egartech.jdbc.starter.util.ConnectionManager;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JdbcRunner {
 
     public static void main(String[] args) throws SQLException {
-        String sql1 = """
-                INSERT into info (data)
-                 VALUES ('Document1'), ('Document2'), ('Document3'), ('Document4') 
-                ;
-                """;
-        String sql2 = """
-                UPDATE info
-                SET data = 'TestTest'
-                WHERE id = 4
-                RETURNING *
+        String sql = """
+                SELECT *
+                FROM docs.documents
                 """;
         try (var connection = ConnectionManager.open();
-             var statement = connection.createStatement()) {
+             var statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
             System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
-            var executeResult = statement.execute(sql2);
-            System.out.println(executeResult);
+
+            var executeResult = statement.executeQuery(sql);
+            while (executeResult.next()) {
+                System.out.println(executeResult.getLong("id"));
+                System.out.println(executeResult.getString("series"));
+                System.out.println(executeResult.getString("number"));
+                System.out.println(executeResult.getString("name"));
+                System.out.println(executeResult.getString("code_type"));
+                System.out.println("------");
+            }
         }
     }
 }
