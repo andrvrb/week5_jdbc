8426a88a2d54c8edf754b659e7d85835810951f6
andrvrb <andr-pnz@mail.ru>
2023-10-12 21:45:10 +0300

    step 6

--- src/main/java/ru/egartech/jdbc/starter/JdbcRunner.java
+++ src/main/java/ru/egartech/jdbc/starter/JdbcRunner.java
@@ -4,29 +4,58 @@ import ru.egartech.jdbc.starter.util.ConnectionManager;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcRunner {
 
     public static void main(String[] args) throws SQLException {
-//        String codeType = "'02' OR '' = ''";
-//        String codeType = "'02' OR '' = ''; DROP TABLE docs.documents";
-        String codeType = " '02' ";
-        var result = getDocsId(codeType);
+/*        Long docId = 2L;
+        var result = getDocById(docId);
+        System.out.println(result);*/
+        var result = getDocsRegBetween(LocalDate.of(2023, 9, 4).atStartOfDay(), LocalDateTime.now());
         System.out.println(result);
     }
 
-    private static List<Long> getDocsId(String codeType) throws SQLException {
+    private static List<Long> getDocsRegBetween(LocalDateTime start, LocalDateTime end) throws SQLException {
+        String sql = """
+                SELECT id
+                FROM docs.document_reg
+                WHERE date_reg BETWEEN ? AND ?
+                """;
+        List<Long> result = new ArrayList<>();
+        try (var connection = ConnectionManager.open();
+             var preparedStatement = connection.prepareStatement(sql)) {
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(start));
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(end));
+            System.out.println(preparedStatement);
+
+            var resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                result.add(resultSet.getLong("id"));
+            }
+        }
+
+        return result;
+    }
+
+    private static List<Long> getDocById(Long docId) throws SQLException {
         String sql = """
                 SELECT id
                 FROM docs.documents
-                WHERE code_type = %s
-                """.formatted(codeType);
+                WHERE id = ?
+                """;
         List<Long> result = new ArrayList<>();
         try (var connection = ConnectionManager.open();
-             var statement = connection.createStatement()) {
-            var resultSet = statement.executeQuery(sql);
+             var preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, docId);
+
+            var resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
 //                result.add(resultSet.getLong("id"));
                 result.add(resultSet.getObject("id", Long.class)); // NULL safe
