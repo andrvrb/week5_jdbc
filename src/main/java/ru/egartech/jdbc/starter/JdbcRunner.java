1e982a624d44405bbcf431b47b145e078711d2d3
andrvrb <andr-pnz@mail.ru>
2023-10-12 21:38:25 +0300

    step 1

--- src/main/java/ru/egartech/jdbc/starter/JdbcRunner.java
+++ src/main/java/ru/egartech/jdbc/starter/JdbcRunner.java
@@ -7,8 +7,30 @@ import java.sql.SQLException;
 public class JdbcRunner {
 
     public static void main(String[] args) throws SQLException {
-        try (var connection = ConnectionManager.open()) {
+        String sql1 = """
+                CREATE DATABASE testttt
+                ;
+                """;
+        String sql2 = """
+                DROP DATABASE testttt
+                ;
+                """;
+        String sql3 = """
+                CREATE schema docs
+                ;
+                """;
+        String sql4 = """
+                CREATE TABLE IF NOT EXISTS info (
+                    id SERIAL PRIMARY KEY ,
+                    data TEXT NOT NULL
+                );
+                """;
+        try (var connection = ConnectionManager.open();
+             var statement = connection.createStatement()) {
+            System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
+            var executeResult = statement.execute(sql4);
+            System.out.println(executeResult);
         }
     }
 }
